de34bb0c44fcc58a7eedc62cfae01a7b74765230
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-11-11 16:58:42 +0530

    Add files via upload

---
+++ testing.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	char app[10];
+	char find;
+	int size;
+	
+	//getting size from user
+	cout<<"enter the number of elements"<<endl;
+	cin>>size;
+	
+	//getting input from user
+	cout<<"enter the "<<size<<" words";
+	for(int i = 0;i<size;i++){
+		cin>>app[i];
+	}
+	
+	//getting the element to be find from user
+	cout<<"enter the element to be found"<<endl;
+	cin>>find;
+	
+	//to check if i have the char in my input
+	if(strchr(app,find))
+		cout<<"present "<<endl;
+	else
+		cout<<"not"<<endl;
+}
