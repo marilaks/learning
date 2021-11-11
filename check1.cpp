63cf9c76adf39a664cd87369a6f9541a350922f9
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-11-11 21:47:07 +0530

    Add files via upload

---
+++ check1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 256;
+	char ch = (char)a + 'a' ;
+	cout<<"value is "<<ch;
+	
+}
+
+/*
+big indian - when we store MSB in least address value
+little indian - when we store LSB in least address value
+int main(){
+	cout<<66<<endl;
+	cout<<(char)67<<endl;
+	cout<<char(68)<<endl;
+}
+*/
